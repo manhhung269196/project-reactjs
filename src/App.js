13eb6c3beb57d12a9ab22aa10463be1485223e13
Dao Manh Hung <hung.dao@hyperlogy.com>
2021-06-03 15:02:01 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+import ListUser from './layout/users/list-user'
+
+function App() {
+  return (
+    <div className="App">
+      <ListUser></ListUser>
+    </div>
+  )
+}
+
+export default App
---
+++ src/layout/users/list-user.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+
+function ListUser()
+{
+    return(
+    <h1>Hello</h1>
+    );
+
+}
+export default ListUser;
